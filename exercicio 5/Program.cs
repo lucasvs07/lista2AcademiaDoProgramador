5fff8cfd3fecb3ddedfe93707886de8a1efb9cef
lucasvs07 <78516282+lucasvs07@users.noreply.github.com>
2022-02-15 12:20:23 -0300

    Add files via upload

---
+++ exercicio 5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex5L2LucasVinicius.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
