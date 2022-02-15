c54ea778d095b100a67ff4bbb8a86d933b551056
lucasvs07 <78516282+lucasvs07@users.noreply.github.com>
2022-02-15 12:19:04 -0300

    Add files via upload

---
+++ exercicio4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ex4L2LucasVinicius.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Faça um algoritmo para receber um número qualquer e informar na tela se
+            é par ou ímpar.*/
+
+            //Apresentação
+            
+            Console.WriteLine("Verifique se um número é par ou impar.");
+            
+            //Entrada de dados e declaração de variáveis.
+
+            Console.WriteLine("Digite um número:");
+            string strnum = Console.ReadLine();
+            double dnum = Convert.ToDouble(strnum);
+
+            //Teste de condições e saída.
+
+            if (dnum % 2 == 0)
+                Console.WriteLine(strnum + " é par.");
+
+            else
+                Console.WriteLine(strnum + " é ímpar.");
+
+            //Finalização.
+
+            Console.WriteLine("Pressione enter para sair.");
+            Console.ReadKey();
+        }
+    }
+}
